063309e304e62b8acd2a394d31da36bbf6f4a942
uminoishi <meimei0128.illustration@gmail.com>
2025-11-12 00:43:02 +0900

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+// memory スライドショー
+
+$('.slider').slick(
+  {
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    fade: true,
+    infinite: true,
+    dots: true,
+    arrows: true,   
+  }
+);
